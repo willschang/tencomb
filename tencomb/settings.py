6e16c159f045d9c6b95f2271a9d24513597a5c8a
willschang <zhhu245@gmail.com>
2017-03-22 09:55:21 +0800

    Hidden the  values of db setting

--- tencomb/settings.py
+++ tencomb/settings.py
@@ -111,27 +111,27 @@ DATABASES = {
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
     # 使用mysql做为数据库（本地测试）
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'tencomb',
-    #     'USER': 'root',
-    #     'PASSWORD': '123456',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
-    #     'STORAGE_ENGINE': 'INNODB',
-    #     'OPTIONS': {},
-    # }
-    # qcloud上数据库
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tencomb',
-        'USER': 'ten',
+        'USER': 'root',
         'PASSWORD': '123456',
-        'HOST': '119.29.40.39',
+        'HOST': 'localhost',
         'PORT': '3306',
         'STORAGE_ENGINE': 'INNODB',
         'OPTIONS': {},
     }
+    # qcloud上数据库
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'tencomb',
+    #     'USER': 'ten',
+    #     'PASSWORD': '123456',
+    #     'HOST': 'ip',
+    #     'PORT': '3306',
+    #     'STORAGE_ENGINE': 'INNODB',
+    #     'OPTIONS': {},
+    # }
 }
 
 
