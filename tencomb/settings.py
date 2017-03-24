3314761d9bf0c77dcd4ac68a16da43a2cd5c6e39
willschang <zhhu245@gmail.com>
2017-03-24 11:13:29 +0800

    Test

--- tencomb/settings.py
+++ tencomb/settings.py
@@ -106,21 +106,21 @@ WSGI_APPLICATION = 'tencomb.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    # 使用mysql做为数据库（本地测试）
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tencomb',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'STORAGE_ENGINE': 'INNODB',
-        'OPTIONS': {},
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 使用mysql做为数据库（本地测试）
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'tencomb',
+    #     'USER': 'root',
+    #     'PASSWORD': '123456',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    #     'STORAGE_ENGINE': 'INNODB',
+    #     'OPTIONS': {},
+    # }
     # qcloud上数据库
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
