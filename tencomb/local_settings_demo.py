cf8e9312b25e37e84394657045f272fea2f38595
willschang <zhhu245@gmail.com>
2017-03-24 13:30:46 +0800

    change db to qserver

--- tencomb/local_settings_demo.py
+++ tencomb/local_settings_demo.py
@@ -2,3 +2,33 @@
 
 # We can overwite setting.py here
 DEBUG = True
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+    # 使用mysql做为数据库（本地测试）
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'tencomb',
+    #     'USER': 'root',
+    #     'PASSWORD': '123456',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    #     'STORAGE_ENGINE': 'INNODB',
+    #     'OPTIONS': {},
+    # }
+    # qcloud上数据库
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'tencomb',
+    #     'USER': 'ten',
+    #     'PASSWORD': '123456',
+    #     'HOST': 'ip',
+    #     'PORT': '3306',
+    #     'STORAGE_ENGINE': 'INNODB',
+    #     'OPTIONS': {},
+    # }
+}
--- tencomb/settings.py
+++ tencomb/settings.py
@@ -106,10 +106,10 @@ WSGI_APPLICATION = 'tencomb.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     # 使用mysql做为数据库（本地测试）
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
@@ -121,17 +121,17 @@ DATABASES = {
     #     'STORAGE_ENGINE': 'INNODB',
     #     'OPTIONS': {},
     # }
-    # qcloud上数据库
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'tencomb',
-    #     'USER': 'ten',
-    #     'PASSWORD': '123456',
-    #     'HOST': 'ip',
-    #     'PORT': '3306',
-    #     'STORAGE_ENGINE': 'INNODB',
-    #     'OPTIONS': {},
-    # }
+    qcloud上数据库
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tencomb',
+        'USER': 'ten',
+        'PASSWORD': '123456',
+        'HOST': '119.29.40.39',
+        'PORT': '3306',
+        'STORAGE_ENGINE': 'INNODB',
+        'OPTIONS': {},
+    }
 }
 
 
